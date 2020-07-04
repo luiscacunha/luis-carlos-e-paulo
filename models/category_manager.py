8530341de068d479d50295bc14f29961ca4adbe7
Paulo Escórcio <pauloescorcio@Escorcio.local>
2020-07-04 13:28:24 +0100

    Identation Fix

--- models/category_manager.py
+++ models/category_manager.py
@@ -6,40 +6,40 @@
 from models.category import category
 
 class categoryManagement:
-        def has_category(self,Category):
-            if category in ["Medicina","Enfermagem","Auxiliar"]:
-                return True
-            return False
+    def has_category(self,Category):
+        if category in ["Medicina","Enfermagem","Auxiliar"]:
+            return True
+        return False
 
-        def add_medicine(self,name):
-            category.medicine.append(name)
+    def add_medicine(self,name):
+        category.medicine.append(name)
 
-        def add_nursing(self,name):
-            category.nursing.append(name)
+    def add_nursing(self,name):
+        category.nursing.append(name)
 
-        def add_helper(self,name):
-            category.helper.append(name)
+    def add_helper(self,name):
+        category.helper.append(name)
 
-        def has_helper(self,name):
-            if name in category.helper:
-                return True
-            return False
+    def has_helper(self,name):
+        if name in category.helper:
+            return True
+        return False
 
-        def has_medicine(self,name):
-            if name in category.medicine:
-                return True
-            return False
+    def has_medicine(self,name):
+        if name in category.medicine:
+            return True
+        return False
         
-        def has_nursing(self,name):
-            if name in category.nursing:
-                return True
-            return False
+    def has_nursing(self,name):
+        if name in category.nursing:
+            return True
+        return False
 
-        def add_professional_in_category(self,Category,name):
-            if Category == "Medicina":
-                self.add_medicine(name)
-            elif category == "Auxiliar":
-                self.add_helper(name)
-            else:
-                self.add_nursing(name)
+    def add_professional_in_category(self,Category,name):
+        if Category == "Medicina":
+            self.add_medicine(name)
+        elif category == "Auxiliar":
+            self.add_helper(name)
+        else:
+            self.add_nursing(name)
             
