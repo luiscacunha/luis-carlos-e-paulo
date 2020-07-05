6aa5e0f3cd4a735afa7608c2aee569221f68032a
Luis Cunha <58328580+luiscacunha@users.noreply.github.com>
2020-07-05 12:46:33 +0100

    Trabalho

--- models/professional.py
+++ models/professional.py
@@ -2,7 +2,7 @@ class professional:
    
    def __init__(self,category,name):
       self.category = category
-      self.name = category
+      self.name = name
 
    def toString(self):
       return "%s %s" %(self.category, self.name)
